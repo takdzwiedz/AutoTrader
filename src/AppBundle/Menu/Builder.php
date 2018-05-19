ead6d17d8dafdb37c9ec8837a4492e0712a5a1be
takdzwiedz <a.kacprzak@tu-i-teraz.com.pl>
2018-05-19 17:39:12 +0200

    Project upload no 2

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A
+ * Date: 16.03.2018
+ * Time: 06:07
+ */
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+    public function mainMenu(MenuFactory $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Home',['route' => 'homepage']);
+        return $menu;
+    }
+}
