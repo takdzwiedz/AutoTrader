ef99a5fd981ed8e1f9fd87a828c1597163fa514f
takdzwiedz <a.kacprzak@tu-i-teraz.com.pl>
2018-05-30 06:17:54 +0200

    form submission, validation, constraints, error dispaly

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -6,20 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/our-cars", name="offer")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
         $cars = $carRepository->findCarsWithDetails();
         $form = $this->createFormBuilder()
             ->setMethod('GET')
-            ->add('search', TextType::class)
+            ->add('search', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2])
+                ]
+            ])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            die('Form submited');
+        }
         return $this->render('CarBundle:Default:index.html.twig',
             [
                 'cars' => $cars,
